d60cbfeba68899615f968c45154fc7d8f2dc1644
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-10-20 20:20:34 +0300

    Start

--- src/Matter.java
+++ src/Matter.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Matter {
+public class Matter {
+    class Atom {
+
+        public int id;
+        public String element;
+
+        public Atom(String elt, int id_) {
+            element = elt;
+            id = id_;
+        }
+
+        @Override
+        public int hashCode() {
+            return id;}        //  Do not modify this method!!
+
+        @Override
+        public boolean equals(Object other) {       //  Do not modify this method!!
+            if (other != null && other instanceof Atom) {
+                Atom that = (Atom) other;
+                return id == that.id;
+            }
+            return false;
+        }
+    }
+
+
+    class Molecule {
+
+    }
 }
